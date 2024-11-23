04c06ebe6f0b5b811e4f90b067ec3c572a212def
VADDU PAVAN KUMAR REDDY <117571020+vaddupavankumarreddy@users.noreply.github.com>
2024-11-23 16:20:07 +0530

    Validators.js

---
+++ react-admin-backend/helpers/Validators.js
@@ -0,0 +1,9 @@
+const {check}=require('express-validator');
+
+exports.registerValidator=[
+    check('name','Name is required').not().isEmpty(),
+    check('email','Please include a valid email').isEmail().normalizeEmail({
+        gmail_remove_dots:true
+    }),
+    check('name','Password is required').not().isEmpty(),s
+]
